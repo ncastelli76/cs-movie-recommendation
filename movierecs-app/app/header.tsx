21756586a0298a1c8b16c8ecae653b79d3bedb9c
ncastelli76 <ncastelli@wpi.edu>
2025-12-01 16:29:03 -0500

    Added basic next.js framework

---
+++ movierecs-app/app/header.tsx
@@ -0,0 +1,47 @@
+
+
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <header className="bg-gray-900 text-white py-4 sticky top-0 z-50">
+      {/* Header container */}
+      <div className="container mx-auto px-4 flex justify-between items-center">
+        {/* Website title */}
+        <h1 className="text-xl font-semibold">Title</h1>
+        {/* Navigation menu */}
+        <nav className="hidden md:block">
+          <ul className="flex gap-x-6">
+            {/* Navigation links */}
+            <li>
+              <Link href="/" className="hover:text-gray-300">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/link1" className="hover:text-gray-300">
+                Browse All
+              </Link>
+            </li>
+            <li>
+              <Link href="/link2" className="hover:text-gray-300">
+                Link2 (optional)
+              </Link>
+            </li>
+            <li>
+              <Link href="/link3" className="hover:text-gray-300">
+                Link3 (optional)
+              </Link>
+            </li>
+            <li>
+              <Link href="/link4" className="hover:text-gray-300">
+                Login
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        {/* Add Mobile Navigation Toggle Here */}
+      </div>
+    </header>
+  );
+}
---
+++ movierecs-app/app/search_bar.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export default function Search_bar() {
+    <div className="bg-gray-900 text-white py-4 z-100">
+      {/* Header container */}
+    <div className="container mx-auto px-4 flex justify-between items-center">
+    <h1 className="text-xl font-semibold">This will handle our search bar</h1></div></div>
+
+}
