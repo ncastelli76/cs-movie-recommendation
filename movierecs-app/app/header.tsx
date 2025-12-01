37fee97c660fec3eede5714865d54c37d8dc3671
ncastelli76 <ncastelli@wpi.edu>
2025-12-01 17:10:42 -0500

    Bugfix

--- movierecs-app/app/header.tsx
+++ movierecs-app/app/header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="bg-gray-900 text-white py-4 sticky top-0 z-50">
+    <header className="bg-neutral-900 text-white py-4 sticky top-0 z-50">
       {/* Header container */}
       <div className="container mx-auto px-4 flex justify-between items-center">
         {/* Website title */}
--- movierecs-app/app/page.tsx
+++ movierecs-app/app/page.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-      <section className= "inner_content bg-grey-100 new_index background_11">
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-neutral-800">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-slate-50 dark:bg-neutral-800 sm:items-start">
+      <section className= "inner_content py-20 new_index background_11">
         <div id="media_v4" className="media discover">
             <div className="column_wrapper">
             <div className="content_wrapper wrap">
                 <div className="title">
-                    <h1>Welcome.</h1>
+                    <h1>This is the main page.</h1>
                     <h2>This section will handle the search engine.</h2>
                 </div>
                 <div className="search"></div>
@@ -17,9 +17,9 @@ export default function Home() {
             </div>
         </div>
       </section>
-      <section className="inner_content trending no_pad"><h2>This section will have a recommendation carousel.</h2></section>
+      <section className="inner_content py-20 trending"><h2>This section will have a recommendation carousel.</h2></section>
 
-      <section className="inner_content trending no_pad"><h2>This section will have an explanation of the purpose of the site.</h2></section>
+      <section className="inner_content py-20 trending no_pad"><h2>This section will have an explanation of the purpose of the site.</h2></section>
       </main>
     </div>
   );
